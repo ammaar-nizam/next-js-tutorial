fa8bd8bd327fc85d2952ba7c42258533430b74b8
ammaar-nizam <ammaar.nizam@outlook.com>
2024-07-12 11:10:34 +0530

    Fetching data; Static and Dynamic Rendering

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -6,12 +6,12 @@ import {
   InvoicesTable,
   LatestInvoiceRaw,
   Revenue,
-} from './definitions';
+} from './definitions'; 
 import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
+    // Artificially delay a response for demo purposes. 
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
